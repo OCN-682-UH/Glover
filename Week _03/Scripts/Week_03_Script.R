0794c060225cbec7f372895e6b4ba0e42704602d
Kent Glover <kaglover@hawaii.edu>
2025-09-09 14:19:33 -1000

    week 3 script

--- Week _03/Scripts/Week_03_Script.R
+++ Week _03/Scripts/Week_03_Script.R
@@ -1,2 +1,177 @@
 # This is my week 3 script going over ggplot.
 
+#load palmer packages
+library(palmerpenguins)
+library(tidyverse)
+glimpse(penguins)
+
+#set data frame
+ggplot(data=penguins)
+
+# add x axis as bill depth
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm))
+
+# add y axis as bill length
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm))
+
+#starting plot
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm)) +
+  geom_point()
+
+#vary color based on species
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species)) +
+  geom_point()
+
+#add title
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species)) +
+  geom_point()+
+  labs(title = "Bill depth and length")
+
+# add subtitle
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species)) +
+  geom_point()+
+  labs(title = "Bill depth and length",
+       subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins")
+
+#add x and y axis labels
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species)) +
+  geom_point()+
+  labs(title = "Bill depth and length",
+       subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+       x = "Bill depth (mm)", y = "Bill length (mm)")
+
+# add legend for species
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species)) +
+  geom_point()+
+  labs(title = "Bill depth and length",
+       subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+       x = "Bill depth (mm)", y = "Bill length (mm)",
+       color = "Species")
+
+# add caption for data source
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species)) +
+  geom_point()+
+  labs(title = "Bill depth and length",
+       subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+       x = "Bill depth (mm)", y = "Bill length (mm)",
+       color = "Species",
+       caption = "Source: Palmer Station LTER / palmerpenguins package")
+
+
+
+#Final Plot choose colorblind friendly theme
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species)) +
+  geom_point()+
+  labs(title = "Bill depth and length",
+       subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+       x = "Bill depth (mm)", y = "Bill length (mm)",
+       color = "Species",
+       caption = "Source: Palmer Station LTER / palmerpenguins package")+
+  scale_color_viridis_d()
+
+
+#add mapping different shape based on island
+
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species,
+                     shape = island
+       )) +
+  geom_point()+
+  labs(title = "Bill depth and length",
+       subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+       x = "Bill depth (mm)", y = "Bill length (mm)",
+       color = "Species") +
+  scale_color_viridis_d()
+
+# mapped shape and color to species
+
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species,
+                     shape = species
+       )) +
+  geom_point()+
+  scale_color_viridis_d()
+
+# change size of points/shapes
+
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species,
+                     size = body_mass_g
+       )) +
+  geom_point()+
+  scale_color_viridis_d()
+
+#increase transparency (alpha)
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species,
+                     size = body_mass_g,
+                     alpha = flipper_length_mm
+       )) +
+  geom_point()+
+  scale_color_viridis_d()
+
+#facet grid plot 
+ggplot(penguins, aes(x = bill_depth_mm,
+    y = bill_length_mm))+
+  geom_point()+
+  facet_grid(species~sex)
+
+# facet wrap plot none based on sex
+ggplot(penguins, aes(x = bill_depth_mm, y = bill_length_mm)) + 
+  geom_point() +
+  facet_wrap(~ species)
+
+# facet plus color
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species,
+       )) +
+  geom_point()+
+  scale_color_viridis_d()+
+  facet_grid(species~sex)
+
+#no legend
+ggplot(data=penguins, 
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
+                     color = species,
+       )) +
+  geom_point()+
+  scale_color_viridis_d()+
+  facet_grid(species~sex)+
+  guides(color = FALSE)
